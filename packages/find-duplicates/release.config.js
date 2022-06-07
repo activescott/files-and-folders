dc3085569736b27b588ad72869947e025be55ce5
Scott Willeke <scott@willeke.com>
2022-06-06 21:36:22 -0700

    build: using .releaserc.json instead of js to avoid esm complexity of node+legacy require loads of semantic release

--- packages/find-duplicates/release.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  // https://github.com/semantic-release/semantic-release/blob/beta/docs/usage/configuration.md
-  branches: [
-    "main",
-    { name: "beta", prerelease: true },
-    { name: "alpha", prerelease: true },
-  ],
-}
